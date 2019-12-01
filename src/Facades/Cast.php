bf5c9c51b18933cc3d5880fc0c32825d525f360a
Jairo Correa <jn.j41r0@gmail.com>
2019-11-30 21:58:43 -0300

    PHPDoc facade

--- src/Facades/Cast.php
+++ src/Facades/Cast.php
@@ -5,6 +5,11 @@ namespace JnJairo\Laravel\Cast\Facades;
 use Illuminate\Support\Facades\Facade;
 use JnJairo\Laravel\Cast\Contracts\Cast as CastContract;
 
+/**
+ * @method static mixed cast($value, string $type, string $format = '')
+ * @method static mixed castDb($value, string $type, string $format = '')
+ * @method static mixed castJson($value, string $type, string $format = '')
+ */
 class Cast extends Facade
 {
     /**
